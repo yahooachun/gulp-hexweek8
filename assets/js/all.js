26e8905065d695ba2aa3c0294b3afbbec4713417
yahooachun <yahooachun@gmail.com>
2021-01-02 15:31:39 +0800

    Update 2021-01-02T07:31:31.370Z

---
+++ assets/js/all.js
@@ -0,0 +1,27 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.navbar-toggler').click(function () {
+    $(this).toggleClass('open');
+  });
+  $('.modal').css('overflow-y', 'auto');
+  $('.dropdown-toggle').dropdown();
+  $('.js-arrow').click(function () {
+    $(this).toggleClass('rotatearrow');
+  });
+  $('.js-carousel').click(function () {
+    var src = $(this).attr('src');
+    $('.js-carousel').css('cursor', 'pointer');
+    $('.js-thumb').attr('src', src);
+  });
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+  AOS.init({
+    once: true,
+    duration: 500
+  });
+});
+//# sourceMappingURL=all.js.map
